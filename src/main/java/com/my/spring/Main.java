1b29470780f323c8415b41496cdf0f3f6942fea3
Keshav Kumar <keshavkumar93761@gmail.com>
2026-02-05 16:13:26 +0530

    update

--- src/main/java/com/my/spring/Main.java
+++ src/main/java/com/my/spring/Main.java
@@ -15,9 +15,11 @@ public class Main {
 		
 		System.out.println(bean);
 		
-//		Employee bean2 = ioc.getBean("emp2",Employee.class);
-//
-//		System.out.println(bean2);
+		Employee bean2 = ioc.getBean("emp2",Employee.class);
+
+		System.out.println(bean2);
+		
+		System.out.println("Main.main()");
 	}
 
 }
